accb1d3d90eefffd99685de54a1ae3771e1f7a12
Robério Almeida <dev.roberio@gmail.com>
2022-02-02 17:52:56 -0300

    1 && 8?

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,11 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (item) => {
+  if (!item) throw new Error('You must provide an url');
+  const url = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`);
+  try {
+    return await url.json();
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,31 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('2 - Fetch foi chamada?', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('3 - Fetch utiliza o endpoint?', async () => {
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+
+  it('4 - É uma estrutura de dados?', async () => {
+    const expected = await fetchProducts('computador');
+    expect(expected).toEqual(computadorSearch);
+  });
+  
+  it('5 - Retorna erro "You must provide an url"?', async () => {
+    try {
+      await fetchProducts();
+    } catch (error) {
+      expect(error).toEqual(new Error('You must provide an url'));
+    }
+  });
 });
