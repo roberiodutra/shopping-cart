28873659c92fe2ccf85be41a2e889b1c377f1f6e
Robério Almeida <dev.roberio@gmail.com>
2022-02-05 00:32:29 -0300

    addeventlistener extra

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
