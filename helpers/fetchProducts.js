037a4586fd4257f8bb8892b48da789b586e1dfa9
Robério Almeida <dev.roberio@gmail.com>
2022-02-05 01:12:03 -0300

    npm test

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,15 +1,9 @@
 const fetchProducts = async (item) => {
   if (!item) throw new Error('You must provide an url');
-  const url = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`);
-  try {
-    return url.json();
-  } catch (error) {
-    return error;
-  }
-  // Poderia fazer assim também.
-  // return fetch(url)
-  //  .then((response) => response.json())
-  //  .catch((error) => error);
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
 };
 
 if (typeof module !== 'undefined') {
