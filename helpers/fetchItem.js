c1df36f00fc7a4ffd037d251a42fac34e2202951
Robério Almeida <dev.roberio@gmail.com>
2022-02-07 18:28:39 -0300

    refatoramento dos testes

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,10 +1,19 @@
-const fetchItem = (itemID) => {
-  if (!itemID) {
+const fetchItem = async (itemID) => {
+  try {
+    const url = `https://api.mercadolibre.com/items/${itemID}`;
+    const response = await fetch(url);
+    return response.json();
+  } catch (error) {
+    //como o retorno do erro é um array, aqui transformo em string, para que no teste, tenha certeza que não é um falso positivo com base no retorno.
     throw new Error('You must provide an url').toString();
   }
-  const url = `https://api.mercadolibre.com/items/${itemID}`;
-  return fetch(url)
-    .then((response) => response.json());
+// Ou assim \/
+//   if (!itemID) {
+//     throw new Error('You must provide an url').toString();
+//   }
+//   const url = `https://api.mercadolibre.com/items/${itemID}`;
+//   return fetch(url)
+//     .then((response) => response.json());
 };
 
 if (typeof module !== 'undefined') {
--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,10 +1,12 @@
-const fetchProducts = (item) => {
-  if (!item) {
+const fetchProducts = async (item) => {
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
+    const response = await fetch(url);
+    return response.json();
+  } catch (error) {
+    //como o retorno do erro é um array, aqui transformo em string, para que no teste, tenha certeza que não é um falso positivo com base no retorno.
     throw new Error('You must provide an url').toString();
   }
-  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
-  return fetch(url)
-    .then((response) => response.json())
 };
 
 if (typeof module !== 'undefined') {
